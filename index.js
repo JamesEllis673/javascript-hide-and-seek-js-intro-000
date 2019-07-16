d5b01dd12fef1c8427735fb9fdb06b2c73f631a6
James Ellis <13jellis@toothillschool.co.uk>
2019-07-16 15:48:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function increaseRankBy (n) {
   const firstList = rankedLists[0]
   const secondList = rankedLists[1]
 
-  let children = firstList.children
-  let start = 1
-  for (let i = 0, l = children.length; i < l; i++) {
+  var children = firstList.children
+  var start = 1
+  for (var i = 0, l = children.length; i < l; i++) {
     children[i].innerHTML = start + i + n
   }
 
   children = secondList.children
   start = 12
 
-  for (let i = 0, l = children.length; i < l; i++) {
+  for (var i = 0, l = children.length; i < l; i++) {
     children[i].innerHTML = start + i + n
 }
