a8f8b3eb19ec4c737cd23d4ef51ab367ac7008c1
James Ellis <13jellis@toothillschool.co.uk>
2019-07-16 15:46:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,19 @@ function deepestChild() {
 }
 
 function increaseRankBy (n) {
-  
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
+
+  let children = firstList.children
+  let start = 1
+  for (let i = 0, l = children.length; i < l; i++) {
+    children[i].innerHTML = start + i + n
+  }
+
+  children = secondList.children
+  start = 12
+
+  for (let i = 0, l = children.length; i < l; i++) {
+    children[i].innerHTML = start + i + n
 }
